b347b917664968f656ebdc700e5e2b7797471779
mohamed-ghareeb <mohamed.ghareeb725@gmail.com>
2024-07-07 13:05:39 +0300

    making the container a singleton

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,25 +1,18 @@
 <?php
 
 use App\Core\App;
-use App\Core\Example;
-use Spatie\Ignition\Ignition;
-use League\Container\Container;
+use App\Core\Container;
 use App\Providers\AppServiceProvider;
 use League\Container\ReflectionContainer;
 
 require '../vendor/autoload.php';
 
-$container = new Container();
+$container = Container::getInstance();
 
 $container->delegate(new ReflectionContainer());
 
 $container->addServiceProvider(new AppServiceProvider());
 
-var_dump($container->get(Example::class));
-die();
-
 $app = new App;
-
-
 
 $app->run();
