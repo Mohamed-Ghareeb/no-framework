06c0c073e1bbcf8a1fce0dd9112d33bd41d36d12
mohamed-ghareeb <mohamed.ghareeb725@gmail.com>
2024-07-20 13:39:35 +0300

    using the services inside the controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,16 +1,19 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\Config\Config;
 use Laminas\Diactoros\Response;
 
 
 class HomeController 
 {
+    public function __construct(protected Config $config) {}
+
     public function __invoke()
     {
         $response = new Response();
 
-        $response->getBody()->write('Home');
+        $response->getBody()->write($this->config->get('app.name'));
 
         return $response;   
     }
