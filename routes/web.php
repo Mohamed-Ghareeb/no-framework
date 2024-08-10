de10d002fcd3ba3d3312dd12decf78a719bd6d2b
mohamed-ghareeb <mohamed.ghareeb725@gmail.com>
2024-08-10 14:43:01 +0300

    create the middlwares and apply it for group of routes

--- routes/web.php
+++ routes/web.php
@@ -8,17 +8,31 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Middleware\RedirectIfAuthenticated;
+use App\Http\Middleware\RedirectIfGuest;
+use League\Route\RouteGroup;
 
-return static function(Router $router, ContainerInterface $container)
-{
+return static function (Router $router, ContainerInterface $container) {
     $router->middleware($container->get('csrf'));
-    
+
     $router->get('/', HomeController::class);
-    $router->get('/dashboard', DashboardController::class);
-    $router->get('/register', [RegisterController::class, 'registerForm']);
-    $router->post('/register', [RegisterController::class, 'register']);
-    $router->get('/login', [LoginController::class, 'loginForm']);
-    $router->post('/login', [LoginController::class, 'login']);
-    $router->post('/logout', LogoutController::class);
+
+    $router->group('/', function (RouteGroup $route) {
+        $route->get('/register', [RegisterController::class, 'registerForm'])
+            ->middleware(new RedirectIfAuthenticated());
+        $route->post('/register', [RegisterController::class, 'register']);
+    
+        $route->get('/login', [LoginController::class, 'loginForm']);
+        $route->post('/login', [LoginController::class, 'login']);
+    })
+    ->middleware(new RedirectIfAuthenticated());
+
+    $router->group('/', function (RouteGroup $route) {
+        $route->get('/dashboard', DashboardController::class)
+            ->middleware(new RedirectIfGuest());
+        $route->post('/logout', LogoutController::class);
+    })
+    ->middleware(new RedirectIfGuest());
+
     $router->get('/users/{user}', UserController::class);
 };
